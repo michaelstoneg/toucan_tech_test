331636877267d86f5f173bb421ab9fff325201fe
michael grandison <michael.grandison@gmail.com>
2017-02-20 00:01:32 +0000

    added index filtering

--- public/js/controllers/membersController.js
+++ public/js/controllers/membersController.js
@@ -8,6 +8,13 @@ function MembersController(Member) {
   members.all = [];
   members.addMember = addMember;
   members.removeMember = removeMember;
+  members.filter = undefined;
+  // members.goFilter = goFilter;
+  //
+  // function goFilter(a) {
+  //   members.filter = a;
+  //   console.log('filter string', members.filter);
+  // }
 
   members.newMember = {};
 
@@ -21,6 +28,27 @@ function MembersController(Member) {
 
     return Member.query(data => {
       members.all = data;
+
+      console.log('all members', members.all);
+
+
+      function sort(arr) {
+        console.log('sorting');
+        arr.sort(function(a, b) {
+          var schoolA=a.school.toLowerCase(), schoolB=b.school.toLowerCase();
+          if (schoolA < schoolB) //sort string ascending
+            return -1;
+          // console.log('sorted', members.all);
+          if (schoolA > schoolB)
+            return 1;
+          return 0; //default return value (no sorting)
+        });
+
+      }
+      sort(members.all, 'school');
+
+
+
     }, onError);
 
   }
@@ -36,5 +64,5 @@ function MembersController(Member) {
   }
 
   getMembers();
-  console.log('members controller');
+  console.log('members controller', 'filter', members.filter);
 }
